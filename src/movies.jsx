73fdfb17126baaa60b63804c03a5331c9cec04fc
Alexandr <alexandr.kopyletz@gmail.com>
2024-09-10 22:17:23 +0300

    10.09/4

--- src/movies.jsx
+++ src/movies.jsx
@@ -5,28 +5,27 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchTrendingMovies = async () => {
   const response = await axios.get(`${BASE_URL}/trending/movie/day`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`
-    }
+    params: {
+      api_key: API_KEY,
+    },
   });
   return response.data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await axios.get(`${BASE_URL}/search/movie`, {
-    params: { query },
-    headers: {
-      Authorization: `Bearer ${API_KEY}`
-    }
+    params: { query,
+      api_key: API_KEY,
+    },
   });
   return response.data.results;
 };
 
 export const fetchMovieDetails = async (movieId) => {
   const response = await axios.get(`${BASE_URL}/movie/${movieId}`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`
-    }
+    params: {
+      api_key: API_KEY,
+    },
   });
   return response.data;
 };
